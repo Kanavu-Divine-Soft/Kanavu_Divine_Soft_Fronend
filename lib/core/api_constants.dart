dfbf1c3419cff32c241cf5ce0dfb7bbf5f6e7b8c
Kalaiyarasan <kalaivelliangiri@gmail.com>
2026-06-24 10:24:39 +0530

    change the base url based on the live

--- lib/core/api_constants.dart
+++ lib/core/api_constants.dart
@@ -4,7 +4,7 @@ class ApiConstants {
   // Update this IP address to your computer's local IP (or 10.0.2.2 for Android Emulator)
   static String get baseUrl {
     if (kIsWeb) {
-      return 'http://localhost:8005';
+      return 'https://kanavu-divine-soft.netlify.app';
     } else {
       return 'http://192.168.68.110:8005';
     }
