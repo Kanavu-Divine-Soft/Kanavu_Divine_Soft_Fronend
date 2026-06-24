9906b42993194693293c481b1a6cd84444834398
Kalaiyarasan <kalaivelliangiri@gmail.com>
2026-06-24 16:14:11 +0530

    fix: conditionally use live backend server URL when building for production

--- lib/core/api_constants.dart
+++ lib/core/api_constants.dart
@@ -4,8 +4,11 @@ class ApiConstants {
   // Update this IP address to your computer's local IP (or 10.0.2.2 for Android Emulator)
   static String get baseUrl {
     if (kIsWeb) {
-      // return 'https://kanavu-divine-soft-backend-33om.onrender.com'; // Live Server
-      return 'http://127.0.0.1:8005'; // Local Server
+      if (kReleaseMode) {
+        return 'https://kanavu-divine-soft-backend-33om.onrender.com'; // Live Server
+      } else {
+        return 'http://127.0.0.1:8005'; // Local Server
+      }
     } else {
       return 'http://192.168.68.110:8005';
     }
