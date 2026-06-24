6b786d63073607bb87e9004c7a3757cdc75c7309
Kalaiyarasan <kalaivelliangiri@gmail.com>
2026-06-24 16:07:11 +0530

    fix: re-track api_constants.dart to resolve Netlify build error

---
+++ lib/core/api_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+
+class ApiConstants {
+  // Update this IP address to your computer's local IP (or 10.0.2.2 for Android Emulator)
+  static String get baseUrl {
+    if (kIsWeb) {
+      // return 'https://kanavu-divine-soft-backend-33om.onrender.com'; // Live Server
+      return 'http://127.0.0.1:8005'; // Local Server
+    } else {
+      return 'http://192.168.68.110:8005';
+    }
+  }
+}
